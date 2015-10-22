9e553e3e3425b54a134d85aa855e5d8af0d1a1a4
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-10-21 21:11:59 -0700

    CriminalIntent Version 1.2
    This marks the completion of Chapter 8. Lab 3-1 is complete.

--- app/src/main/java/com/brandon/android/criminalintent/Crime.java
+++ app/src/main/java/com/brandon/android/criminalintent/Crime.java
@@ -1,16 +1,20 @@
 package com.brandon.android.criminalintent;
 
 import java.util.UUID;
+import java.util.Date;
 
 public class Crime
 {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime()
     {
         //Generate a unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId()
@@ -26,5 +30,25 @@ public class Crime
     public String getTitle()
     {
         return mTitle;
+    }
+
+    public void setDate(Date date)
+    {
+        mDate = date;
+    }
+
+    public Date getDate()
+    {
+        return mDate;
+    }
+
+    public boolean isSolved()
+    {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved)
+    {
+        mSolved = solved;
     }
 }
