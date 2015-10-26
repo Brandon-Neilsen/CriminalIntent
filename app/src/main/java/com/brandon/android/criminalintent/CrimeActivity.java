5eedb3d1d3b8589318bbc273ef5ddd65d6aecebd
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-10-26 11:45:03 -0700

    CriminalIntent Version 2.2.6
    Listing 10.6 Complete

--- app/src/main/java/com/brandon/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/brandon/android/criminalintent/CrimeActivity.java
@@ -3,11 +3,14 @@ package com.brandon.android.criminalintent;
 import android.view.MenuItem;
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment(){
-        return new CrimeFragment();
+        UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 
     @Override
