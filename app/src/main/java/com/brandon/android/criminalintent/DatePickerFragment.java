efa2d519004f269b5358c2aa072abbb3c56b076c
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-10-28 16:49:56 -0700

    CriminalIntent Version 3.2.5
    Listing 12.5 Complete

--- app/src/main/java/com/brandon/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/brandon/android/criminalintent/DatePickerFragment.java
@@ -6,17 +6,29 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
+    public static final String EXTRA_DATE = "com.brandon.android.criminalintent.date";
+    private Date mDate;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
         View v = getActivity().getLayoutInflater()
                 .inflate(R.layout.dialog_date, null);
-        
+
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
     }//end onCreateDialog(Bundle)
+    public static DatePickerFragment newInstance(Date date){
+        Bundle args = new  Bundle();
+        args.putSerializable(EXTRA_DATE, date);
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 }//end DatePickerFragment class
