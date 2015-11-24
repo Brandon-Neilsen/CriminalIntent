18e5166d8dffe17cc0f5bba7e3775d373528ad17
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-11-24 12:29:07 -0800

    CriminalIntent Version 4.7
    Listing 16.7 Complete

--- app/src/main/java/com/brandon/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/brandon/android/criminalintent/CrimeListFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.ListFragment;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -55,6 +56,21 @@ public class CrimeListFragment extends ListFragment {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_crime_list, menu);
     }//end onCreateOptionsMenu(Menu, MenuInflater)
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_item_new_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+                i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
+                startActivityForResult(i, 0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }//end onOptionsItemSelected(MenuItem)
 
     private class CrimeAdapter extends ArrayAdapter<Crime> {
         public CrimeAdapter(ArrayList<Crime> crimes){
