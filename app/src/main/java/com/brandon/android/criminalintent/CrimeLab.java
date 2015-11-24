58e40bfc0b2c2b3b5def4f90633bc2e7155d8562
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-11-24 12:25:09 -0800

    CriminalIntent Version 4.6
    Listing 16.6 Complete

--- app/src/main/java/com/brandon/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/brandon/android/criminalintent/CrimeLab.java
@@ -12,12 +12,6 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
-        for (int i = 0; i < 100;i++){
-            Crime c = new Crime();
-            c.setTitle("Crime #" +(i + 1));
-            c.setSolved(i % 2 == 0);
-            mCrimes.add(c);
-        }
     }
 
     public static CrimeLab get(Context c) {
