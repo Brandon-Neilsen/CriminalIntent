ea533d4285c17b47c1c718961360bc56d9dd6c50
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-11-25 11:02:03 -0800

    CriminalIntent Version 5.1.3
    -Listing 17.3 Complete
    -Edited comments for stylistic reasons

--- app/src/main/java/com/brandon/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/brandon/android/criminalintent/CrimeLab.java
@@ -1,10 +1,17 @@
 package com.brandon.android.criminalintent;
 
 import android.content.Context;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class CrimeLab {
+
+    private static final String TAG = "CrimeLab";
+    private static final String FILENAME = "crimes.json";
+
+    private CriminalIntentJSONSerializer mSerializer;
     private ArrayList<Crime> mCrimes;
     private static CrimeLab sCrimeLab;
     private Context mAppContext;
@@ -12,7 +19,8 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
-    }
+        mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
+    }//end CrimeLab(Context)
 
     public static CrimeLab get(Context c) {
         if (sCrimeLab == null) {
@@ -38,4 +46,15 @@ public class CrimeLab {
         }
         return null;
     }//end getCrime(UUID)
+
+    public boolean saveCrimes() {
+        try {
+            mSerializer.saveCrimes(mCrimes);
+            Log.d(TAG, "Crimes saved to file.");
+            return true;
+        } catch (Exception e){
+            Log.e(TAG, "Error saving crimes", e);
+            return false;
+        }
+    }
 }//end CrimeLab class
