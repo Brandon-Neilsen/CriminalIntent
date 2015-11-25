ddf73eae80106ec9cb7e62dfedc4d6c61cafe589
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-11-25 13:27:32 -0800

    CriminalIntent Version 5.1.7
    -Listing 17.7 Complete
    -Removed an unused import statement from
    CriminalIntentJSONSerializer.java

--- app/src/main/java/com/brandon/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/brandon/android/criminalintent/CrimeLab.java
@@ -18,8 +18,13 @@ public class CrimeLab {
 
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
-        mCrimes = new ArrayList<Crime>();
         mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
+        try {
+            mCrimes = mSerializer.loadCrimes();
+        }catch (Exception e){
+            mCrimes = new ArrayList<Crime>();
+            Log.e(TAG, "Error loading crimes: ", e);
+        }
     }//end CrimeLab(Context)
 
     public static CrimeLab get(Context c) {
--- app/src/main/java/com/brandon/android/criminalintent/CriminalIntentJSONSerializer.java
+++ app/src/main/java/com/brandon/android/criminalintent/CriminalIntentJSONSerializer.java
@@ -13,7 +13,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Writer;
-import java.nio.channels.FileLockInterruptionException;
 import java.util.ArrayList;
 
 public class CriminalIntentJSONSerializer {
