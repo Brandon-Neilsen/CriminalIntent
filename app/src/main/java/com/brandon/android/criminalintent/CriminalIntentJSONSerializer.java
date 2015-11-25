4236bb5f5a3c5d6d8e200282c86c94ec712fb671
Brandon Neilsen <brandon.e.neilsen@gmail.com>
2015-11-25 13:24:28 -0800

    CriminalIntent Version 5.1.6
    Listing 17.6 Complete

--- app/src/main/java/com/brandon/android/criminalintent/CriminalIntentJSONSerializer.java
+++ app/src/main/java/com/brandon/android/criminalintent/CriminalIntentJSONSerializer.java
@@ -4,10 +4,16 @@ import android.content.Context;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONTokener;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Writer;
+import java.nio.channels.FileLockInterruptionException;
 import java.util.ArrayList;
 
 public class CriminalIntentJSONSerializer {
@@ -18,6 +24,35 @@ public class CriminalIntentJSONSerializer {
         mContext = c;
         mFileName = f;
     }//end CriminalIntentJSONSerializer(Context, String)
+
+    public ArrayList<Crime> loadCrimes() throws JSONException, IOException {
+        ArrayList<Crime> crimes = new ArrayList<>();
+        BufferedReader reader = null;
+        try {
+            //Open and read th file into a StringBuilder
+            InputStream in = mContext.openFileInput(mFileName);
+            reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder jsonString = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                //Line breaks are omitted and irrelevant
+                jsonString.append(line);
+            }
+            //Pare the JSON using JSONTokener
+            JSONArray array = (JSONArray) new JSONTokener(jsonString.toString()).nextValue();
+            //Build the array of crimes from JSONObjects
+            for (int i = 0; i< array.length(); i++){
+                crimes.add(new Crime(array.getJSONObject(i)));
+            }
+        }catch (FileNotFoundException e){
+            //Ignore this one; it happens when starting fresh.
+        }finally {
+            if (reader != null){
+                reader.close();
+            }
+        }
+        return crimes;
+    }//end loadCrimes()
 
     public void saveCrimes(ArrayList<Crime> crimes) throws JSONException, IOException {
         //Build an array in JSON
